786d51beeb41fe3e0ddbe5572c7ae15215078dcf
Thiago Moreira <loganguns@gmail.com>
2014-10-20 03:53:33 -0200

    Api Fix

--- config/autoload/app.local.php
+++ config/autoload/app.local.php
@@ -14,12 +14,12 @@ return array(
         'app' => array ( 
         		'45893' => array (
         				'name' => 'MarkSend',
-        				'url' => 'http://clickmarkdigital.com/',
+        				'url' => 'http://marksend.dev/',
         				'token' => '4e1650a3a4d9a5e8a879011bcecbc262'
         		),
         		'79216' => array (
         				'name' => 'ClickMark',
-		        		'url' => 'http://clickmarkdigital.com/',
+		        		'url' => 'http://clickmark.dev/',
 		        		'token' => 'c6fd603ccc5ff5898097dd4ed0d2a93a'
 		        	),
         )
